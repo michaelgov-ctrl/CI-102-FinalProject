2c7ebacae06f933973f1a04458a9527df3af1b52
Michael Governanti <michaelgovernanti@gmail.com>
2025-01-29 13:38:19 -0500

    WithKey(), added flag

--- main.go
+++ main.go
@@ -8,8 +8,9 @@ import (
 )
 
 func main() {
-	var targetDirectories string
+	var targetDirectories, key string
 	flag.StringVar(&targetDirectories, "target_directories", "", "[required] Comma-separated list of directories to encrypt")
+	flag.StringVar(&key, "key", "", "Key to use for encryption/decryption")
 
 	var workerCount int
 	flag.IntVar(&workerCount, "worker_count", 5, "Number of workers to encrypt/decrpyt with")
@@ -26,7 +27,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	cryptor := NewCryptor(WithWorkerCount(workerCount), WithEncryption(encrypt))
+	cryptor := NewCryptor(WithWorkerCount(workerCount), WithEncryption(encrypt), WithKey([]byte(key)))
 
 	cryptor.EnumerateDirectories(dirs)
 
